42ea723bf6fc06c27c4262ae70163080e7bc64f7
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-06-25 07:34:40 +0200

    Build the essential files structure & install the core dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "airbnb",
+  rules: {
+    "linebreak-style": ["error", "windows"],
+    "no-console": "off",
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }],
+    "react/react-in-jsx-scope": "off"
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+  },
+};
